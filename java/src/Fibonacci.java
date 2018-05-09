7d4bbccb548aa3694db61e1263763a34317c7236
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-08 22:23:41 -0300

    Fibonacci origin

--- java/src/Fibonacci.java
+++ java/src/Fibonacci.java
@@ -1,7 +1,5 @@
-import java.util.Scanner;
 public class Fibonacci {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
 		int fib=0, ant=1;
 		
 		for(int i=1; i<=10; i++) {
