07982372ebba25e86d187f564ff3a325828a1f0c
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-12 12:32:14 -0300

    Q01...

--- java/src/listavetores/Q1.java
+++ java/src/listavetores/Q1.java
@@ -1,10 +1,22 @@
 package listavetores;
+import java.util.Scanner;
 
 public class Q1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner leia = new Scanner(System.in);
+		
+		int[] num = new int[10];
+		
+		
+		for (int i = 0; i < num.length; i++) {
+			System.out.printf("%dº num: ",(i + 1));
+			num[i] = read.nextInt();
+		}
+		
+		for (int i = 0; i < num.length; i++) {
+			System.out.printf("%dº num: %d\n",(i + 1),num[i]);
+		}
 	}
 
 }
