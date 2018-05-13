a8738e6cc7bac1d825d8279afb8a30d621123d67
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-13 15:21:29 -0300

    Q1666666

--- java/src/listavetores/Q16.java
+++ java/src/listavetores/Q16.java
@@ -7,17 +7,34 @@ public class Q16 {
 		Scanner read = new Scanner(System.in);
 		
 		int num[] = new int[4];
-		int qnt = 0, c = 0;
+		int qntDif = 0;
 		
 		for (int i = 0; i < num.length; i++) {
 			System.out.printf("%dº num: ",(i + 1));
 			num[i] = read.nextInt();
-			
-			for (int j = 0; j < num.length; j++) {
-				if (num[i] == (num[j])) {
-					c++;
-				}
+		}
+		
+		for (int i = 0, j = 0; j < num.length; j++) {
+			if ((num[i] == (num[j]) && (j == 0))) {
+				++qntDif;
+			} else if (num[i] != num[j]){
+				++qntDif;
 			}
+		}
+		
+		int num2[] = new int[qntDif];
+		
+		for (int i = 0, j = 0; j < num2.length; j++) {
+			if (num[i] != num[j]) {
+				num2[i] = num[j];
+				++i;
+			}
+		}
+		
+		System.out.printf("\nQnt. dif: %d,\n\nÍndice\t\tValor\n\n",qntDif);
+		
+		for (int i = 0; i < num2.length; i++) {
+			System.out.printf("[%d]\t-\t%d\n",i,num2[i]);
 		}
 		
 		
