f8d9f49623438d414adef7370a4be0c131707683
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-15 09:29:30 -0300

    Q16git add java/src/listavetores/Q16.javagit add java/src/listavetores/Q16.javagit add java/src/listavetores/Q16.java

--- java/src/listavetores/Q16.java
+++ java/src/listavetores/Q16.java
@@ -6,7 +6,7 @@ public class Q16 {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		
-		int num[] = new int[4];
+		int num[] = new int[10];
 		int qntDif = 0;
 		
 		for (int i = 0; i < num.length; i++) {
@@ -15,26 +15,40 @@ public class Q16 {
 		}
 		
 		for (int i = 0, j = 0; j < num.length; j++) {
-			if ((num[i] == (num[j]) && (j == 0))) {
+			if (j == 0) {
 				++qntDif;
+			} else if ((num[i] != num[j])){
+				for (int k = (j + 1); k < num.length; k++) {
+					if (num[j] == num[k]) {
+						--qntDif;
+					}
+				}
+				++qntDif;
+			}
+		}
+		
+		int dif[] = new int[qntDif];
+
+		for (int i = 0, j = 0, k = 0; j < (dif.length); j++) {
+			if (j == 0) {
+				dif[k] = j;
+				++k;
 			} else if (num[i] != num[j]){
-				++qntDif;
+				dif[k] = j;
+				++k;
 			}
 		}
 		
 		int num2[] = new int[qntDif];
 		
-		for (int i = 0, j = 0; j < num2.length; j++) {
-			if (num[i] != num[j]) {
-				num2[i] = num[j];
-				++i;
-			}
+		for (int i = 0; i < num2.length; i++) {
+			num2[i] = num[dif[i]];
 		}
 		
-		System.out.printf("\nQnt. dif: %d,\n\nÍndice\t\tValor\n\n",qntDif);
+		System.out.println();
 		
 		for (int i = 0; i < num2.length; i++) {
-			System.out.printf("[%d]\t-\t%d\n",i,num2[i]);
+			System.out.printf("[%d]\n",num2[i]);
 		}
 		
 		
