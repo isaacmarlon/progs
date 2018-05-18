98b960daee0aab34181c6c3b7f13c5a1f442e635
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-17 21:01:49 -0300

    Q17 - Finished

--- java/src/listavetores/Q17.java
+++ java/src/listavetores/Q17.java
@@ -6,17 +6,17 @@ public class Q17 {
 	public static void main(String[] args) {
 		Scanner read = new Scanner(System.in);
 		
-		int a1[] = new int[4];
-		int a2[] = new int[4];
-		int a3[] = new int[8];
+		String a1[] = new String[4];
+		String a2[] = new String[4];
+		String idx[] = new String[8];
 		
 		for (int i = 0, pass = 1; pass != 3; i++) {
 			if (pass == 1) {
 				System.out.printf("%dº (a1): ", (i + 1));
-				a1[i] = read.nextInt();
+				a1[i] = read.nextLine();
 			} else {
 				System.out.printf("%dº (a2): ", (i + 1));
-				a2[i] = read.nextInt();
+				a2[i] = read.nextLine();
 			}
 			if (i == (a1.length - 1)) {
 				++pass;
@@ -24,56 +24,44 @@ public class Q17 {
 				System.out.println();
 			}
 		}
-		
-		/*for (int i = 0, j = 0, k = 0; i < a1.length; j++) {
-			if (j < a1.length) {
-				a3[i] = a1[j];
-				++i;
-			} else { 
-				a3[j] = a2[k];
-				++k;
-				++i;
-			}
-		}*/
-		
-		int doisVetores = a1.length;                              		
+
 		int qntIguais = 0;
+		boolean rep = false;
 		
-		for (int i = 0, j = 0; j < doisVetores; j++) {
-			if ((a1[i] == a2[j])){
-				/*for (int k = (j + 1); k < a1.length; k++) {
-					if (a1[j] != a1[k]) {
-						--qntIguais;
+		for (int i = 0, cIdx = 0; i < a1.length; i++) {
+			for (int q = (i + 1); q < a1.length; q++) {
+				if (a1[i].equals(a1[q])) 	++i;
+			}
+			
+			for (int j = 0; j < a1.length; j++) {
+				if ((a1[i].equals(a2[j]))) {
+					for (int k = 0; k < a1.length; k++) {
+						if (a1[i].equals(idx[k])) {
+							rep = true;
+						}
 					}
-				}*/
-				++qntIguais;
+					
+					if(!rep) {
+						idx[cIdx] = a1[i];
+						++cIdx;
+						++qntIguais;
+					} else {
+						rep = false;
+					}
+				}
 			}
 		}
 		
-		int iguais[] = new int[qntIguais];
 		
-		for (int i = 0, j = 0, k = 0; k < iguais.length; j++) {
-			if (j == 0) {
-				iguais[k] = j;
-				++k;
-			} else if (a1[i] != a1[j]){
-				iguais[k] = j;
-				++k;
-			}
+		String a3[] = new String[qntIguais];
+		
+		for (int i = 0; i < qntIguais; i++) {
+			a3[i] = idx[i];
 		}
 		
-		int ax[] = new int[qntIguais];
-		
-		for (int i = 0; i < ax.length; i++) {
-			ax[i] = a1[iguais[i]];
+		for (int i = 0; i < a3.length; i++) {
+			System.out.printf("[%s]\n",a3[i]);
 		}
-		
-		System.out.println();
-		
-		for (int i = 0; i < ax.length; i++) {
-			System.out.printf("[%d]\n",ax[i]);
-		}
-		
 		
 	}
 
