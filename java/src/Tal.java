2bac8352f08890ae055d4a8d2dda92c6db32c181
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-05-27 15:35:31 -0300

    Q

---
+++ java/src/Tal.java
@@ -0,0 +1,28 @@
+public class Tal {
+
+	public static void main(String[] args) {
+		int m[][] = new int[5][5];
+		
+		for (int i = -1, j = -1; m[4][0] != 1;) {
+			if (i == 2) { ++m[j][i]; }
+			if (i < (m.length - 1) && (m[4][4] != 1)) {
+				++i;
+				++j;
+			}
+			else if ((j == i) && (i > 0) j = 0;
+			else if (i >= 0) {
+				++j;
+				--i;
+			}
+			if ((i >= 0) && (i != 2)) { m[j][i] = 1; } 
+			
+		}
+		
+		for (int i = 0; i < m.length; i++) {
+			for (int j = 0; j < m[i].length; j++) {
+				System.out.printf("[%d]",m[i][j]);
+			}
+			System.out.println();
+		}
+	}
+}
--- java/src/listaMatrizes/Q10.java
+++ java/src/listaMatrizes/Q10.java
@@ -6,23 +6,112 @@ public class Q10 {
 	public static void main(String[] args) {
 		Scanner leia = new Scanner(System.in);
 		
-		float m[][] = new float[2][4];
+		final int LINHAS = 2;
+		final int COLUNAS = 4;
 		
-		for (int i = 0; i < m.length; i++) {
-			for (int j = 0; j < m[i].length; j++) {
+		float m[][] = new float[LINHAS][COLUNAS];
+		
+		// LEITURA DA MATRIZ
+		
+		for (int i = 0; i < LINHAS; i++) {
+			for (int j = 0; j < COLUNAS; j++) {
 				System.out.printf("m[%d][%d]: ",i,j);
 				m[i][j] = leia.nextFloat();
 			}
+			System.out.println();
 		}
 		
-		float maiores[] = new float[m.length];
+		// MAIORES ELEMENTOS DAS LINHAS
 		
-		for (int i = 0, j = 0; i < m.length; i++) {
-			for (int k = 0; k < m[i].length; k++) {
-				if (i == 0) maiores[j] = m[i][k];
-				else if (m[i][k] > maiores[j])	maiores[j] = m[i][k];
+		float maioresLinha[] = new float[LINHAS];
+		
+		for (int i = 0, j = 0; i < LINHAS; i++) {
+			for (int k = 0; k < COLUNAS; k++) {
+				if (k == 0) maioresLinha[j] = m[i][k];
+				else if (m[i][k] > maioresLinha[j])	maioresLinha[j] = m[i][k];
+			}
+			++j;
+		}
+		
+		// MAIORES ELEMENTOS DAS COLUNAS
+		
+		float maioresColunas[] = new float[COLUNAS];
+		
+		for (int i = 0, j = 0; i < COLUNAS; i++) {
+			for (int k = 0; k < LINHAS; k++) {
+				if (k == 0)	maioresColunas[j] = m[k][i];
+				else if (m[k][i] > maioresColunas[j]) maioresColunas[j] = m[k][i];
+			}
+			++j;
+		}
+		
+		// IMPRIMINDO MATRIZ
+		
+		System.out.println("m:");
+		
+		for (int i = 0; i < LINHAS; i++) {
+			for (int j = 0; j < COLUNAS; j++) {
+				System.out.printf("[%.2f]",m[i][j]);
+			}
+			System.out.println();
+		}
+		
+		// IMPRIMINDO MAIORES ELEMENTOS DAS LINHAS
+		
+		System.out.println("\nmaioresLinha:");
+		
+		for (int i = 0; i < maioresLinha.length; i++) {
+			System.out.printf("[%.2f]",maioresLinha[i]);
+		}
+		
+		// IMPRIMINDO MAIORES ELEMENTOS DAS COLUNAS
+		
+		System.out.println("\n\nmaioresColunas:");
+		
+		for (int i = 0; i < maioresColunas.length; i++) {
+			System.out.printf("[%.2f]",maioresColunas[i]);
+		}
+		
+		// SOMA DAS COLUNAS
+		
+		float somaColunas[] = new float[COLUNAS];
+		
+		for (int i = 0, j = 0; i < COLUNAS; i++) {
+			for (int k = 0; k < LINHAS; k++) {
+				somaColunas[j] += m[k][i];
+			}
+			++j;
+		}
+		
+		// IMPRIMINDO SOMA DAS COLUNAS
+		
+		System.out.println("\n\nsomaColunas:");
+		
+		for (int i = 0; i < COLUNAS; i++) {
+			System.out.printf("[%.2f]", somaColunas[i]);
+		}
+		
+		// PRODUTO
+		
+		float produto = 1;
+		
+		for (int i = 0; i < LINHAS; i++) {
+			for (int j = 0; j < COLUNAS; j++) {
+				if (m[i][j] != 0) produto *= m[i][j];
 			}
 		}
+		
+		int qntNegativos = 0;
+		
+		for (int i = 0; i < LINHAS; i++) {
+			for (int j = 0; j < COLUNAS; j++) {
+				if (m[i][j] < 0) ++qntNegativos;
+			}
+		}
+		
+		
+		System.out.printf("\n\nProduto: %.2f\n",produto);
+		System.out.printf("qntNegativos: %d\n",qntNegativos);
 	}
 
 }
