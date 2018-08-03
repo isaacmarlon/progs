8822adf0a857b018b2c4390f805ca8fc4cfef08a
Isaac Marlon <isaacmarlondasilvalourenco@gmail.com>
2018-08-03 09:05:23 -0300

    project DudeRobot

---
+++ java/src/Dudes/src/DudeRobot.java
@@ -0,0 +1,33 @@
+/**
+ *
+ * @author isaacmsl;
+ * 
+ */
+
+public class DudeRobot {
+    
+    private String name;
+    private int age;
+    
+    public DudeRobot() {
+        name = "N/A";
+        age = 0;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+    
+}
---
+++ java/src/Dudes/src/DudeRobotTest.java
@@ -0,0 +1,70 @@
+/**
+ *
+ * @author isaacmsl;
+ *
+ */
+
+import java.util.Random;
+
+public class DudeRobotTest {
+    
+    static DudeRobot robots[] = new DudeRobot[5];
+    static Random random = new Random();
+    
+    static String[] firstNames = {"Carl","Max","Steven","Bryan","Clodis","Bebe","Tico","Gus","Walt","Jesse","Mike"};
+    static String[] lastNames =  {"Musk","Richard","Andersson","Smith","Jackson","Ground","Bis","Yellow","John","Black"};
+    
+    public static void initialize() {
+        
+        for (int i = 0; i < robots.length; i++) {
+            
+            robots[i] = new DudeRobot();
+            
+        }
+        
+    }
+    
+    public static void setNames() {
+        
+        for (DudeRobot robot : robots) {
+            
+            robot.setName(firstNames[random.nextInt(firstNames.length)]);
+            robot.setName(robot.getName() + " " + lastNames[random.nextInt(lastNames.length)]);
+            
+        }
+        
+    }
+    
+    public static void setAges(int maxAge) {    
+        
+        for (DudeRobot robot : robots) {
+            
+            robot.setAge(random.nextInt(maxAge) + 1);
+            
+        }
+    }
+    
+    public static void printAges() {
+        
+        for (DudeRobot robot : robots) {
+            
+            System.out.println(robot.getName() + " - ");
+            System.out.println("Age: " + robot.getAge() + "\n");
+            
+        }
+        
+    }
+
+    public static void main(String[] args) {
+        
+        initialize();
+        
+        setNames();
+        
+        setAges(10);
+        
+        printAges();
+
+    }
+
+}
